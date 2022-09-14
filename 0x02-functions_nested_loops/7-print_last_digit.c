9fa6ce052611e0abd9543e7544393e84ccd8c63e
Jay-kas <kasumujohnson.o@gmail.com>
2022-09-13 18:39:01 -0700

    task 1

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_last_digit -> prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: the last digit
+*/
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n < 0)
+		n = -n;
+
+	x = n % 10;
+
+	if (x < 0)
+		x = -x;
+
+	_putchar (x + '0');
+
+	return (x);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
