2563c8ebbe1f63c16d91bf846a0e39c819881e1a
Jay-kas <kasumujohnson.o@gmail.com>
2022-09-26 19:03:04 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+include "main.h"
 
 /**
   * _strstr - locate a substring
@@ -32,37 +32,4 @@ char *_strstr(char *haystack, char *needle)
 	}
 
 	return ('\0');
-}
-Chidiebere Amuchionu9:54 PM
-5
-linda gift9:55 PM
-lipsealed
-Denyefa Leader9:58 PM
-ALLENNNNNNNNNNNNN
-To wicked am just hungry me lol
-Uloka Ngozi9:59 PM
-Sir i have a challenge
-Engr Poama10:05 PM
-TASK 7
-include "main.h"
-
-/**
-  * print_chessboard - Print the chessboard
-  * @a: array of pieces
-  *
-  * Return: Nothing.
-  */
-void print_chessboard(char (*a)[8])
-{
-	int b, c;
-
-	for (b = 0; b < 8; b++)
-	{
-		for (c = 0; c < 8; c++)
-		{
-			_putchar(a[b][c]);
-		}
-
-		_putchar('\n');
-	}
 }
