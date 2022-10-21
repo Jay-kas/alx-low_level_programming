fc8c767c632461777759af289f87260f57f9cd4a
Jay-kas <kasumujohnson.o@gmail.com>
2022-10-21 09:13:55 -0700

    task task

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -4,18 +4,18 @@
 #include "lists.h"
 
 /**
- * main -check the code for Holberton school studentss.
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-int main (void)
+int main(void)
 {
-	list_t *head;
-	list_t *new;
-	list_t hello = {"world", 5, NULL};
-	size_t n;
+    list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
+    size_t n;
 
-	head = &hello;
+    head = &hello;
     new = malloc(sizeof(list_t));
     if (new == NULL)
     {
@@ -38,5 +38,3 @@ int main (void)
     free(new);
     return (0);
 }
-
-
