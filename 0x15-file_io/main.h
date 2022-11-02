846c9fa0ea4adb4be8a09f0267b63ce04be6a9d3
Jay-kas <kasumujohnson.o@gmail.com>
2022-11-01 17:42:20 -0700

    README

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,17 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
+/* open */
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+
+/* close */
 #include <unistd.h>
 
-int _putchar(char c);
+/* dprintf */
+#include <stdio.h>
+
+/* malloc, free */
+#include <stdlib.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif
